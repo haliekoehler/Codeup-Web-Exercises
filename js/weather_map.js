551704ff27b910b2516ce2f3d5e3c1fa86ef25ed
haliekoehler <koehlerdoom@gmail.com>
2016-12-06 15:51:29 -0600

    Weather Applicaton exercise + lat/long fields + city, state fields

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,20 +3,66 @@
  */
 "use strict";
 
-var weatherDiv = $('#weatherDiv');
-
-
-
 // get current weather data & store in variable
 var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-        APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
-        q: "San Antonio, TX",
-        units: "imperial",
-        cnt: 3
+    APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
+    q: "San Antonio, TX",
+    units: "imperial",
+    cnt: 3
+});
+
+// do this once data is received
+weatherData.done(function (data) {
+
+    // console.log for reference
+    console.log(data);
+    console.log(data.list);
+
+    // put name of city into header
+    $('#cityName').text(data.city.name);
+
+    // put number of days into header
+    $('#dayCount').text(data.cnt);
+
+    // var date = moment.unix(obj.dt);
+    var days = '';
+
+    data.list.forEach(function (obj) {
+        days +=
+            "<div class='dayDiv'>"
+            + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+            + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
+            + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
+            + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
+            + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
+            + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+            + "</div>";
+
+        $('#weatherDiv').html(days);
     });
 
-    // do this once data is received
-    weatherData.done(function (data) {
+    // put HTML into target div
+    $('#weatherDiv').html(days);
+
+
+}); // end of weatherData.done function
+
+
+$('#latLongBtn').click(function () {
+
+    var newLat = $('#lat').val();
+    var newLong = $('#long').val();
+
+    console.log(newLat);
+    console.log(newLong);
+
+    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
+        lat: newLat,
+        lon: newLong,
+        units: "imperial",
+        cnt: 3
+    }).done(function (data) {
 
         // console.log for reference
         console.log(data);
@@ -31,7 +77,7 @@ var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily",
         // var date = moment.unix(obj.dt);
         var days = '';
 
-        data.list.forEach(function(obj){
+        data.list.forEach(function (obj) {
             days +=
                 "<div class='dayDiv'>"
                 + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
@@ -39,7 +85,7 @@ var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily",
                 + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
                 + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
                 + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
-                + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon +".png'>"
+                + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
                 + "</div>";
 
             $('#weatherDiv').html(days);
@@ -47,55 +93,52 @@ var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily",
 
         // put HTML into target div
         $('#weatherDiv').html(days);
+    });
 
+});
 
+    $('#cityStateBtn').click(function () {
 
-      }); // end of weatherData.done function
+        var newCity = $('#cityState').val();
 
-// weatherData.done(function(data){
-//    console.log(data.list);
-//
-//     // make a variable for the date
-//     // var date = moment.unix(object.dt);
-//
-//     // make variable for storing HTML
-//     var populateForecast = "<div>"
-//         // + "<h2>Date: " + date.format("ddd, MM-DD-YYYY") + "</h2>"
-//         + "<div><p> High - Low: " + object.temp.max + "/" + object.temp.min + "</p>"
-//         + "<p>Conditions: " +object.weather[0].main + "</p>"
-//         + "<p>Humidity: " + object.humidity + "</p>"
-//         + "<p>Wind Speed: " + object.speed + "</p></div>";
-//
-//     // empty string that will be filled with populated forecasts
-//     var days = '';
-//
-//     // populateForecast for each object in data.list
-//     data.list.forEach(function(obj){
-//         days += populateForecast;
-//         $('.row').html(populateForecast);
-//     })
-// });
+    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+            APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
+            q: newCity,
+            units: "imperial",
+            cnt: 3
+        }).done(function (data) {
 
-        // function populate(obj, i, high, low, conditions, humidity, wind, pressure){
-        //     return '<div>'
-        //         + high + "/" + low
-        //         + 'Conditions: ' + conditions
-        //         + 'Humidity: ' + humidity
-        //         + 'Wind: ' + wind
-        //         + 'Pressure: ' + pressure
-        // }
-        //
-        // data.list.forEach(function(obj, i){
-        //     var forecast = populate(
-        //         object, i,
-        //         data.list[i].temp.max,
-        //         data.list[i].temp.min,
-        //         data.list[i].weather[i].description
-        //     )
-        // })
+            // put name of city into header
+            $('#cityName').text(data.city.name);
 
+            // put number of days into header
+            $('#dayCount').text(data.cnt);
 
+            // var date = moment.unix(obj.dt);
+            var days = '';
 
+            data.list.forEach(function (obj) {
+                days +=
+                    "<div class='dayDiv'>"
+                    + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+                    + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
+                    + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
+                    + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
+                    + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
+                    + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+                    + "</div>";
+
+                $('#weatherDiv').html(days);
+            });
+
+            // put HTML into target div
+            $('#weatherDiv').html(days);
+
+            // clear input value
+            $('#cityState').val('')
+        });
+
+    });
 
 
 
