ca94e55498336534e98b99ac961b012a033a4273
haliekoehler <koehlerdoom@gmail.com>
2016-12-07 21:19:08 -0600

    Weather Application exercise + map added + styling in progress

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,9 +1,9 @@
-/**
- * Created by HKoehler on 12/5/16.
- */
+// /*** Created by HKoehler on 12/5/16.*/
+
 "use strict";
 
-// get current weather data & store in variable
+
+// ---- DEFAULT: San Antonio Weather Data ------------------------------//
 var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
     APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
     q: "San Antonio, TX",
@@ -11,7 +11,7 @@ var weatherData = $.get("http://api.openweathermap.org/data/2.5/forecast/daily",
     cnt: 3
 });
 
-// do this once data is received
+// ----- DEFAULT: Weather Data .done Function ----------------------------------//
 weatherData.done(function (data) {
 
     // console.log for reference
@@ -24,30 +24,41 @@ weatherData.done(function (data) {
     // put number of days into header
     $('#dayCount').text(data.cnt);
 
-    // var date = moment.unix(obj.dt);
+    // do this for each day
     var days = '';
 
     data.list.forEach(function (obj) {
         days +=
             "<div class='dayDiv'>"
-            + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+            + "<h4 class='dateText'>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
             + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
             + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
             + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
             + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
-            + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+            + "<img class='icon' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
             + "</div>";
 
         $('#weatherDiv').html(days);
     });
 
-    // put HTML into target div
-    $('#weatherDiv').html(days);
+
+    // data.list.forEach(function (obj) {
+    //     var day = "<div class='dayDiv'>"
+    //         + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+    //         + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
+    //         + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
+    //         + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
+    //         + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
+    //         + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+    //         + "</div>";
+    //
+    //     // put HTML into target div
+    //     $('#weatherDiv').append(day);
 
 
-}); // end of weatherData.done function
+}); // -------- end of weatherData.done function
 
-
+// ---- LAT/LONG BUTTON FUNCTION ----------------------------------- //
 $('#latLongBtn').click(function () {
 
     var newLat = $('#lat').val();
@@ -74,6 +85,42 @@ $('#latLongBtn').click(function () {
         // put number of days into header
         $('#dayCount').text(data.cnt);
 
+        var days = '';
+
+        data.list.forEach(function (obj) {
+            days +=
+                "<div class='dayDiv'>"
+                + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+                + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
+                + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
+                + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
+                + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
+                + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+                + "</div>";
+
+            $('#weatherDiv').html(days);
+        });
+    });
+
+}); //end of latLongBtn click function
+
+$('#cityStateBtn').click(function () {
+
+    var newCity = $('#cityState').val();
+
+    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
+        q: newCity,
+        units: "imperial",
+        cnt: 3
+    }).done(function (data) {
+
+        // put name of city into header
+        $('#cityName').text(data.city.name);
+
+        // put number of days into header
+        $('#dayCount').text(data.cnt);
+
         // var date = moment.unix(obj.dt);
         var days = '';
 
@@ -93,56 +140,79 @@ $('#latLongBtn').click(function () {
 
         // put HTML into target div
         $('#weatherDiv').html(days);
+
+        // clear input value
+        $('#cityState').val('')
     });
 
+}); // end of cityStateBtn click function
+
+// ----- MAP OPTIONS ----------------------------------------------------//
+var mapOptions = {
+    // ZOOM LEVEL
+    zoom: 5,
+    // DEFAULT CENTER [SA,TX]
+    center: {
+        lat: 29.42412,
+        lng: -98.493629
+    }
+    // // SCROLLWHEEL OFF
+    // scrollwheel: false,
+};
+
+
+// ---- RENDER MAP -----------------------------------------------------//
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+
+// ---- DEFAULT: CREATE MARKER ---------------------//
+var marker = new google.maps.Marker({
+    position: {
+        lat: 29.42412,
+        lng: -98.493629
+    },
+    map: map,
+    animation: google.maps.Animation.DROP,
+    draggable: true
 });
 
-    $('#cityStateBtn').click(function () {
+google.maps.event.addListener(marker, 'dragend', function(event) {
+    var dragLat = marker.getPosition().lat();
+    var dragLng = marker.getPosition().lng();
 
-        var newCity = $('#cityState').val();
+    console.log(dragLat);
+    console.log(dragLng);
 
-    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-            APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
-            q: newCity,
-            units: "imperial",
-            cnt: 3
-        }).done(function (data) {
-
-            // put name of city into header
-            $('#cityName').text(data.city.name);
-
-            // put number of days into header
-            $('#dayCount').text(data.cnt);
-
-            // var date = moment.unix(obj.dt);
-            var days = '';
-
-            data.list.forEach(function (obj) {
-                days +=
-                    "<div class='dayDiv'>"
-                    + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
-                    + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
-                    + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
-                    + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
-                    + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
-                    + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
-                    + "</div>";
-
-                $('#weatherDiv').html(days);
-            });
-
-            // put HTML into target div
-            $('#weatherDiv').html(days);
-
-            // clear input value
-            $('#cityState').val('')
-        });
-
+    var updateFromMap = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        APPID: "1aa73ecf55c9a516cfc7ffeb4fb3613f",
+        lat: dragLat,
+        lng: dragLng,
+        units: "imperial",
+        cnt: 3
     });
 
+    updateFromMap.done(function (data) {
 
+        // put name of city into header
+        $('#cityName').html("New Location" + (data.city.coord.lat) + " / " + (data.city.coord.lng));
 
+        var days = '';
 
+        data.list.forEach(function (obj) {
+            days +=
+                "<div class='dayDiv'>"
+                + "<h4>" + moment.unix(obj.dt).format("ddd, MMM-DD-YYYY") + "</h4>"
+                + "<p><strong>High:</strong> " + Math.round(obj.temp.max) + "&deg" + " /// " + "<strong>Low:</strong> " + Math.round(obj.temp.min) + "&deg" + "</p>"
+                + "<p><strong>Conditions:</strong> " + obj.weather[0].main + " ( " + obj.weather[0].description + " )</p>"
+                + "<p><strong>Humidity:</strong> " + obj.humidity + "</p>"
+                + "<p><strong>Pressure:</strong> " + obj.pressure + "</p>"
+                + "<img class='center-block' src='http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png'>"
+                + "</div>";
+
+            $('#weatherDiv').html(days);
+        });
+    });
+});
 
 
 
